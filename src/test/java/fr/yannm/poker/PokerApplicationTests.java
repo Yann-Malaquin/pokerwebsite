d8df9d998d10622a9c7089f6b0a63e6db547df6c
Yann Malaquin <yann.malaquin@gmail.com>
2021-07-23 14:53:10 +0200

    :tada: initial commit

---
+++ src/test/java/fr/yannm/poker/PokerApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.yannm.poker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PokerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
