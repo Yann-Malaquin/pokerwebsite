248ad2f8ed2122978f4e5071d9dd72ab117e4e66
Yann Malaquin <yann.malaquin@gmail.com>
2021-08-13 14:39:59 +0200

    :sparkles: création de la récupération de mot de passe

---
+++ src/app/_services/user.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+import {HttpClient, HttpHeaders} from "@angular/common/http";
+import {Observable} from "rxjs";
+import {environment} from "../../environments/environment";
+
+const HTTPOPTIONS = {
+  headers: new HttpHeaders({'Content-Type': 'application/json'})
+};
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+
+  constructor(private http: HttpClient) {
+  }
+
+  recovery(email: string, password: string): Observable<any> {
+    return this.http.put(`${environment.url}/recovery`, {
+      email,
+      password
+    }, HTTPOPTIONS);
+  }
+}
